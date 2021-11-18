06d181569c6d2b9b1fa603d06a0b78aa2729b5c3
Sjors van Holst <sjorsvanholst@gmail.com>
2021-11-18 22:40:29 +0100

    :bug: Fixed SQL bug

--- repository/PlayerRepository.ts
+++ repository/PlayerRepository.ts
@@ -26,12 +26,12 @@ export default class PlayerRepository implements InterfaceRepository {
     const promises = [];
 
     promises.push(this.mysqlClient.execute(
-      `SELECT HEX(player.uuid) AS uuid, HEX(player.server) AS server, player.created, player.updated, SUM(CASE WHEN entry.giveable = 1 THEN entry.sips ELSE 0 END) AS sips_giveable, SUM(CASE WHEN entry.giveable = 1 THEN entry.shots ELSE 0 END) AS shots_giveable, SUM(CASE WHEN entry.giveable = 0 THEN entry.sips ELSE 0 END) AS sips_remaining, SUM(CASE WHEN entry.giveable = 0 THEN entry.shots ELSE 0 END) AS shots_remaining, -SUM(CASE WHEN entry.sips < 0 AND entry.giveable = 0 THEN entry.sips ELSE 0 END) as sips_taken, -SUM(CASE WHEN entry.shots < 0 AND entry.giveable = 0 THEN entry.shots ELSE 0 END) as shots_taken FROM player INNER JOIN entry ON player.uuid = entry.player WHERE player.server = UNHEX(REPLACE(?, '-', '')) AND entry.server = UNHEX(REPLACE(?, '-', '')) GROUP BY player.uuid, player.server ORDER BY player.created DESC LIMIT ? OFFSET ?`,
+      `SELECT HEX(player.uuid) AS uuid, HEX(player.server) AS server, player.created, player.updated, SUM(CASE WHEN entry.giveable = 1 THEN entry.sips ELSE 0 END) AS sips_giveable, SUM(CASE WHEN entry.giveable = 1 THEN entry.shots ELSE 0 END) AS shots_giveable, SUM(CASE WHEN entry.giveable = 0 THEN entry.sips ELSE 0 END) AS sips_remaining, SUM(CASE WHEN entry.giveable = 0 THEN entry.shots ELSE 0 END) AS shots_remaining, -SUM(CASE WHEN entry.sips < 0 AND entry.giveable = 0 THEN entry.sips ELSE 0 END) as sips_taken, -SUM(CASE WHEN entry.shots < 0 AND entry.giveable = 0 THEN entry.shots ELSE 0 END) as shots_taken FROM player LEFT JOIN entry ON player.uuid = entry.player WHERE player.server = UNHEX(REPLACE(?, '-', '')) AND entry.server = UNHEX(REPLACE(?, '-', '')) GROUP BY player.uuid, player.server ORDER BY player.created DESC LIMIT ? OFFSET ?`,
       [server, server, limit, offset],
     ));
 
     promises.push(this.mysqlClient.execute(
-      `SELECT COUNT(player.uuid) AS total FROM player INNER JOIN entry ON player.uuid = entry.player WHERE player.server = UNHEX(REPLACE(?, '-', '')) AND entry.server = UNHEX(REPLACE(?, '-', ''))`,
+      `SELECT COUNT(player.uuid) AS total FROM player LEFT JOIN entry ON player.uuid = entry.player WHERE player.server = UNHEX(REPLACE(?, '-', '')) AND entry.server = UNHEX(REPLACE(?, '-', ''))`,
       [server, server],
     ));
 
@@ -118,8 +118,8 @@ export default class PlayerRepository implements InterfaceRepository {
 
   public async getObject(uuid: string, server: string): Promise<PlayerEntity> {
     const data = await this.mysqlClient.execute(
-      `SELECT HEX(player.uuid) AS uuid, HEX(player.server) AS server, player.created, player.updated, SUM(CASE WHEN entry.giveable = 1 THEN entry.sips ELSE 0 END) AS sips_giveable, SUM(CASE WHEN entry.giveable = 1 THEN entry.shots ELSE 0 END) AS shots_giveable, SUM(CASE WHEN entry.giveable = 0 THEN entry.sips ELSE 0 END) AS sips_remaining, SUM(CASE WHEN entry.giveable = 0 THEN entry.shots ELSE 0 END) AS shots_remaining, -SUM(CASE WHEN entry.sips < 0 AND entry.giveable = 0 THEN entry.sips ELSE 0 END) as sips_taken, -SUM(CASE WHEN entry.shots < 0 AND entry.giveable = 0 THEN entry.shots ELSE 0 END) as shots_taken FROM player INNER JOIN entry ON player.uuid = entry.player WHERE player.uuid = UNHEX(REPLACE(?, '-', '')) AND player.server = UNHEX(REPLACE(?, '-', '')) AND entry.server = UNHEX(REPLACE(?, '-', '')) GROUP BY player.uuid, player.server`,
-      [uuid, server, server],
+      `SELECT HEX(player.uuid) AS uuid, HEX(player.server) AS server, player.created, player.updated, SUM(CASE WHEN entry.giveable = 1 THEN entry.sips ELSE 0 END) AS sips_giveable, SUM(CASE WHEN entry.giveable = 1 THEN entry.shots ELSE 0 END) AS shots_giveable, SUM(CASE WHEN entry.giveable = 0 THEN entry.sips ELSE 0 END) AS sips_remaining, SUM(CASE WHEN entry.giveable = 0 THEN entry.shots ELSE 0 END) AS shots_remaining, -SUM(CASE WHEN entry.sips < 0 AND entry.giveable = 0 THEN entry.sips ELSE 0 END) as sips_taken, -SUM(CASE WHEN entry.shots < 0 AND entry.giveable = 0 THEN entry.shots ELSE 0 END) as shots_taken FROM player LEFT JOIN entry ON player.uuid = entry.player WHERE player.uuid = UNHEX(REPLACE(?, '-', '')) AND player.server = UNHEX(REPLACE(?, '-', '')) GROUP BY player.uuid, player.server`,
+      [uuid, server],
     );
 
     if (typeof data.rows === "undefined" || data.rows.length === 0) {
