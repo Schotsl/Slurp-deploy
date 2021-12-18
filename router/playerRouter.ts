c05c0cdd98adc8f168e8cca12bc7ce077b2d9333
Sjors van Holst <sjorsvanholst@gmail.com>
2021-12-18 02:34:39 +0100

    :poop: Added WS to router

--- router/playerRouter.ts
+++ router/playerRouter.ts
@@ -9,15 +9,33 @@ const playerController = new PlayerController(
   mysqlClient,
   "player",
 );
+import manager from "../manager.ts";
 
 const get = playerController.getCollection.bind(playerController);
 const post = playerController.addObject.bind(playerController);
 const remove = playerController.removeObject.bind(playerController);
 
-playerRouter.use(authenticationHandler);
+// playerRouter.use(authenticationHandler);
 
 playerRouter.get("/", get);
 playerRouter.post("/", post);
 playerRouter.delete("/:uuid", remove);
+
+playerRouter.get("/ws", async (ctx) => {
+  if (!ctx.isUpgradable) {
+    throw new Error("Connection is not upgradable!");
+  }
+
+  const socket = await ctx.upgrade();
+  manager.addClient(socket);
+  // socket.onopen = () => {
+  //   console.log("Websocket on open!");
+  //   socket.send("Hey! This is a websocket!");
+  // };
+
+  // socket.onmessage = (msg) => {
+  //   console.log("MSG:", msg);
+  // };
+});
 
 export default playerRouter;
