5137c1a6cdb463a6459733e2ec364681e8c7717b
GitHub <noreply@github.com>
2022-11-16 00:17:20 +0000

    ⬆️ Update dependencies

--- collection/PlayerCollection.ts
+++ collection/PlayerCollection.ts
@@ -1,4 +1,4 @@
-import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.0/collection/BaseCollection.ts";
+import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.1/collection/BaseCollection.ts";
 import PlayerEntity from "../entity/PlayerEntity.ts";
 
 export default class PlayerCollection extends BaseCollection {
